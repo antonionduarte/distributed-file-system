abffa42cdcb571527f006248ac7566519d5e2e58
daro-almeida <daro.almeida@campus.fct.unl.pt>
2022-04-15 19:27:50 +0100

    Directory Resource and Directory Web Service

--- src/tp1/server/rest/resources/DirectoryResource.java
+++ src/tp1/server/rest/resources/DirectoryResource.java
@@ -1,51 +1,114 @@
 package tp1.server.rest.resources;
 
 import jakarta.inject.Singleton;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 import tp1.api.FileInfo;
 import tp1.api.service.rest.RestDirectory;
-import util.Discovery;
-import tp1.server.rest.UsersServer;
+import tp1.api.service.util.Directory;
+import tp1.api.service.util.Result;
+import tp1.server.JavaDirectory;
+import util.ConvertError;
 
-import java.net.URI;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @Singleton
 public class DirectoryResource implements RestDirectory {
 
-	private static final Discovery discovery = Discovery.getInstance();
+	final Directory impl = new JavaDirectory();
 
 	@Override
 	public FileInfo writeFile(String filename, byte[] data, String userId, String password) {
-		ArrayList<URI> userServerURI = discovery.knownUrisOf(UsersServer.SERVICE);
+		Result<FileInfo> result;
+		try {
+			result = impl.writeFile(filename,data,userId,password);
+		} catch (MalformedURLException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
 
-
-
-		return null;
+		if (result.isOK()) {
+			return result.value();
+		}
+		else {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 
 	@Override
 	public void deleteFile(String filename, String userId, String password) {
+		Result<Void> result;
+		try {
+			result = impl.deleteFile(filename,userId,password);
+		} catch (MalformedURLException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
 
+		if (!result.isOK()) {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 
 	@Override
 	public void shareFile(String filename, String userId, String userIdShare, String password) {
+		Result<Void> result;
+		try {
+			result = impl.shareFile(filename,userId,userIdShare,password);
+		} catch (MalformedURLException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
 
+		if (!result.isOK()) {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 
 	@Override
 	public void unshareFile(String filename, String userId, String userIdShare, String password) {
+		Result<Void> result;
+		try {
+			result = impl.unshareFile(filename,userId,userIdShare,password);
+		} catch (MalformedURLException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
 
+		if (!result.isOK()) {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 
 	@Override
 	public byte[] getFile(String filename, String userId, String accUserId, String password) {
-		return new byte[0];
+		Result<byte[]> result;
+		try {
+			result = impl.getFile(filename,userId,accUserId,password);
+		} catch (MalformedURLException e) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+
+		if (result.isOK()) {
+			return result.value();
+		}
+		else {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 
 	@Override
 	public List<FileInfo> lsFile(String userId, String password) {
-		return null;
+		Result<List<FileInfo>> result  = impl.lsFile(userId,password);
+
+		if (result.isOK()) {
+			return result.value();
+		}
+		else {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
 	}
 }
--- src/tp1/server/soap/services/SoapDirectoryWebService.java
+++ src/tp1/server/soap/services/SoapDirectoryWebService.java
@@ -4,39 +4,91 @@ import jakarta.jws.WebService;
 import tp1.api.FileInfo;
 import tp1.api.service.soap.DirectoryException;
 import tp1.api.service.soap.SoapDirectory;
+import tp1.api.service.util.Directory;
+import tp1.api.service.util.Result;
+import tp1.server.JavaDirectory;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 @WebService(serviceName = SoapDirectory.NAME, targetNamespace = SoapDirectory.NAMESPACE, endpointInterface = SoapDirectory.INTERFACE)
 public class SoapDirectoryWebService implements SoapDirectory {
 
+    final Directory impl = new JavaDirectory();
+
     @Override
     public FileInfo writeFile(String filename, byte[] data, String userId, String password) throws DirectoryException {
-        return null;
+        Result<FileInfo> result;
+        try {
+            result = impl.writeFile(filename,data,userId,password);
+        } catch (MalformedURLException e) {
+            throw new DirectoryException(Result.ErrorCode.BAD_REQUEST.toString());
+        }
+
+        if (result.isOK())
+            return result.value();
+        else
+            throw new DirectoryException(result.error().toString());
     }
 
     @Override
     public void deleteFile(String filename, String userId, String password) throws DirectoryException {
-
+        Result<Void> result;
+        try {
+            result = impl.deleteFile(filename, userId,password);
+        } catch (MalformedURLException e) {
+            throw new DirectoryException(Result.ErrorCode.BAD_REQUEST.toString());
+        }
+        if(!result.isOK())
+            throw new DirectoryException(result.error().toString());
     }
 
     @Override
     public void shareFile(String filename, String userId, String userIdShare, String password) throws DirectoryException {
-
+        Result<Void> result;
+        try {
+            result = impl.shareFile(filename, userId, userIdShare, password);
+        } catch (MalformedURLException e) {
+            throw new DirectoryException(Result.ErrorCode.BAD_REQUEST.toString());
+        }
+        if(!result.isOK())
+            throw new DirectoryException(result.error().toString());
     }
 
     @Override
     public void unshareFile(String filename, String userId, String userIdShare, String password) throws DirectoryException {
-
+        Result<Void> result;
+        try {
+            result = impl.unshareFile(filename, userId, userIdShare, password);
+        } catch (MalformedURLException e) {
+            throw new DirectoryException(Result.ErrorCode.BAD_REQUEST.toString());
+        }
+        if(!result.isOK())
+            throw new DirectoryException(result.error().toString());
     }
 
     @Override
     public byte[] getFile(String filename, String userId, String accUserId, String password) throws DirectoryException {
-        return new byte[0];
+        Result<byte[]> result;
+        try {
+            result = impl.getFile(filename,userId,accUserId,password);
+        } catch (MalformedURLException e) {
+            throw new DirectoryException(Result.ErrorCode.BAD_REQUEST.toString());
+        }
+
+        if (result.isOK())
+            return result.value();
+        else
+            throw new DirectoryException(result.error().toString());
     }
 
     @Override
     public List<FileInfo> lsFile(String userId, String password) throws DirectoryException {
-        return null;
+        Result<List<FileInfo>> result = impl.lsFile(userId,password);
+
+        if (result.isOK())
+            return result.value();
+        else
+            throw new DirectoryException(result.error().toString());
     }
 }
