9a21ac4ff81064006905f896fb557f5095ffa2bb
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-04-21 19:51:21 +0000

    Finished caches

--- src/tp1/clients/ClientFactory.java
+++ src/tp1/clients/ClientFactory.java
@@ -1,5 +1,9 @@
 package tp1.clients;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import jakarta.ws.rs.client.Client;
+import tp1.api.User;
 import tp1.api.service.util.Directory;
 import tp1.api.service.util.Files;
 import tp1.api.service.util.Users;
@@ -13,33 +17,67 @@ import util.Discovery;
 import util.Pair;
 
 import java.net.MalformedURLException;
-import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class ClientFactory {
 
 	private static final Discovery discovery = Discovery.getInstance();
 
-	public static Pair<String, Users> getUsersClient() throws MalformedURLException {
-		var serverURI = discovery.knownUrisOf("users").get(0); // use discovery to find a uri of the Users service;
-		if (serverURI.toString().endsWith("rest"))
-			return new Pair<>(serverURI.toString(), new RestUsersClient(serverURI));
-		else
-			return new Pair<>(serverURI.toString(), new SoapUsersClient(serverURI));
+	private static final int CACHE_DURATION = 10; // 10 seconds for now
+
+	Cache<String, Pair<String, Users>> usersCache;
+	Cache<String, Pair<String, Files>> filesCache;
+	Cache<String, Pair<String, Directory>> directoryCache;
+
+	public static ClientFactory getInstance() {
+		if (instance == null) {
+			instance = new ClientFactory();
+		}
+		return instance;
 	}
 
-	public static Pair<String, Directory> getDirectoryClient() throws MalformedURLException {
-		var serverURI = discovery.knownUrisOf("directory").get(0); // use discovery to find a uri of the Users service;
-		if (serverURI.toString().endsWith("rest"))
-			return new Pair<>(serverURI.toString(), new RestDirectoryClient(serverURI));
-		else
-			return new Pair<>(serverURI.toString(), new SoapDirectoryClient(serverURI));
+	private static ClientFactory instance;
+
+	public ClientFactory() {
+		this.usersCache = CacheBuilder.newBuilder().expireAfterAccess(CACHE_DURATION, TimeUnit.SECONDS).build();
+		this.filesCache = CacheBuilder.newBuilder().expireAfterAccess(CACHE_DURATION, TimeUnit.SECONDS).build();
+		this.directoryCache = CacheBuilder.newBuilder().expireAfterAccess(CACHE_DURATION, TimeUnit.SECONDS).build();
 	}
 
-	public static Pair<String, Files> getFilesClient() throws MalformedURLException {
-		var serverURI = discovery.knownUrisOf("files").get(0); // use discovery to find a uri of the Users service;
-		if (serverURI.toString().endsWith("rest"))
-			return new Pair<>(serverURI.toString(), new RestFilesClient(serverURI));
-		else
-			return new Pair<>(serverURI.toString(), new SoapFilesClient(serverURI));
+	public Pair<String, Users> getUsersClient() throws MalformedURLException, ExecutionException {
+		var serverURI = discovery.knownUrisOf("users").get(0);
+
+		return this.usersCache.get(serverURI.toString(), () -> {
+			if (serverURI.toString().endsWith("rest")) {
+				return new Pair<>(serverURI.toString(), new RestUsersClient(serverURI));
+			} else {
+				return new Pair<>(serverURI.toString(), new SoapUsersClient(serverURI));
+			}
+		});
+	}
+
+	public Pair<String, Directory> getDirectoryClient() throws MalformedURLException, ExecutionException {
+		var serverURI = discovery.knownUrisOf("directory").get(0); // use discovery to find an uri of the Users service;
+
+		return this.directoryCache.get(serverURI.toString(), () -> {
+			if (serverURI.toString().endsWith("rest")) {
+				return new Pair<>(serverURI.toString(), new RestDirectoryClient(serverURI));
+			} else {
+				return new Pair<>(serverURI.toString(), new SoapDirectoryClient(serverURI));
+			}
+		});
+	}
+
+	public Pair<String, Files> getFilesClient() throws MalformedURLException, ExecutionException {
+		var serverURI = discovery.knownUrisOf("files").get(0); // use discovery to find an uri of the Users service;
+
+		return this.filesCache.get(serverURI.toString(), () -> {
+			if (serverURI.toString().endsWith("rest")) {
+				return new Pair<>(serverURI.toString(), new RestFilesClient(serverURI));
+			} else {
+				return new Pair<>(serverURI.toString(), new SoapFilesClient(serverURI));
+			}
+		});
 	}
 }
