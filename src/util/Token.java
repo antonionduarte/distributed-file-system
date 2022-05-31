4f8e47d1c9fe1ff556adac606e523db07425b961
daro-almeida <daro.almeida@campus.fct.unl.pt>
2022-05-31 22:28:05 +0100

    changed Token class

--- src/util/Token.java
+++ src/util/Token.java
@@ -1,42 +1,42 @@
 package util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
 
 public class Token {
 
 	private Token() {}
 
-	public static String generate(String secret, String... args) {
+	static MessageDigest md;
 
+	static {
 		try {
-			ByteArrayOutputStream os = new ByteArrayOutputStream();
-			for (String arg : args) {
-				os.write(arg.getBytes());
-			}
-			long currentTimeInSeconds = System.currentTimeMillis()/1000;
-			os.write((byte) currentTimeInSeconds);
-			os.write(secret.getBytes());
-
-			return SHAsum(os.toByteArray());
-		} catch (IOException | NoSuchAlgorithmException ex) {
-			throw new RuntimeException(ex);
+			md = MessageDigest.getInstance("SHA256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static String SHAsum(byte[] convertMe) throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance("SHA-1");
-		return byteArray2Hex(md.digest(convertMe));
+	synchronized private static byte[] digest(byte[] data) {
+		md.reset();
+		md.update(data);
+		return md.digest();
 	}
 
-	private static String byteArray2Hex(final byte[] hash) {
-		Formatter formatter = new Formatter();
-		for (byte b : hash) {
-			formatter.format("%02x", b);
-		}
-		return formatter.toString();
+	public static String generate(byte[] data) {
+		return String.format("%016X", new BigInteger(1,digest(data)));
+	}
+
+	public static String generate(String data) {
+		return generate( data.getBytes() );
+	}
+
+	synchronized public static String generate(Object ...values) {
+		md.reset();
+		for( var o : values )
+			md.update( o.toString().getBytes() );
+
+		return String.format("%016X", new BigInteger(1, md.digest()));
 	}
 }
