3b59eab011191321638d6a185d35b9eeb3d15829
antonionduarte <an.duarte@campus.fct.unl.pt>
2022-05-31 22:04:51 +0000

    JavaFilesProxy things

---
+++ src/tp1/server/rest/FilesProxyServer.java
@@ -0,0 +1,53 @@
+package tp1.server.rest;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import tp1.server.rest.resources.FilesProxyResource;
+import tp1.server.rest.resources.FilesResource;
+import tp1.server.util.CustomLoggingFilter;
+import tp1.server.util.GenericExceptionMapper;
+import util.Debug;
+import util.Discovery;
+
+import javax.net.ssl.SSLContext;
+import java.net.InetAddress;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FilesProxyServer {
+
+	public static final int PORT = 8080;
+	public static final String SERVICE = "files";
+	private static final Logger Log = Logger.getLogger(FilesProxyServer.class.getName());
+	private static final String SERVER_URI_FMT = "https://%s:%s/rest";
+
+	static {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+	}
+
+	public static void main(String[] args) {
+		try {
+			Debug.setLogLevel(Level.INFO, Debug.SD2122);
+
+			ResourceConfig config = new ResourceConfig();
+			String token = args[0];
+			config.register(new FilesProxyResource());
+			config.register(CustomLoggingFilter.class);
+			config.register(GenericExceptionMapper.class);
+
+			String ip = InetAddress.getLocalHost().getHostAddress();
+			String serverURI = String.format(SERVER_URI_FMT, ip, PORT);
+			JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config, SSLContext.getDefault());
+
+			Log.info(String.format("%s Server ready @ %s\n", SERVICE, serverURI));
+
+			Discovery discovery = Discovery.getInstance();
+			discovery.start(SERVICE, serverURI);
+
+			// More code can be executed here...
+		} catch (Exception e) {
+			Log.severe(e.getMessage());
+		}
+	}
+}
---
+++ src/tp1/server/rest/resources/FilesProxyResource.java
@@ -0,0 +1,53 @@
+package tp1.server.rest.resources;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import tp1.api.service.rest.RestFiles;
+import tp1.api.service.util.Files;
+import tp1.server.JavaFilesProxy;
+import util.ConvertError;
+
+@Singleton
+public class FilesProxyResource implements RestFiles {
+
+	private final Files impl;
+
+	public FilesProxyResource() {
+		this.impl = new JavaFilesProxy();
+	}
+
+	@Override
+	public void writeFile(String fileId, byte[] data, String token) {
+		var result = impl.writeFile(fileId, data, token);
+
+		if (!result.isOK()) {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		} else {
+			throw new WebApplicationException(Response.Status.NO_CONTENT);
+		}
+	}
+
+	@Override
+	public void deleteFile(String fileId, String token) {
+		var result = impl.deleteFile(fileId, token);
+
+		if (!result.isOK()) {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
+	}
+
+	@Override
+	public byte[] getFile(String fileId, String token) {
+		var result = impl.getFile(fileId, token);
+
+		if (result.isOK()) {
+			return result.value();
+		} else {
+			var errorCode = ConvertError.resultErrorToWebAppError(result);
+			throw new WebApplicationException(errorCode);
+		}
+	}
+}
