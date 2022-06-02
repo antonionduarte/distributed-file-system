8b4cf26683957b7a5a51cc9b5b353552c224e22a
daro-almeida <daro.almeida@campus.fct.unl.pt>
2022-06-02 01:33:59 +0100

    cringe

--- src/tp1/server/rest/resources/FilesProxyResource.java
+++ src/tp1/server/rest/resources/FilesProxyResource.java
@@ -13,6 +13,10 @@ public class FilesProxyResource implements RestFiles {
 
 	private final Files impl;
 
+	public FilesProxyResource() {
+		this.impl = new JavaFilesProxy(false, null, null, null);
+	}
+
 	public FilesProxyResource(boolean deleteAll, String apiSecret, String apiKey, String accessKey) {
 		this.impl = new JavaFilesProxy(deleteAll, apiSecret, accessKey, apiKey);
 	}
